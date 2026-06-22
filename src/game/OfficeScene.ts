88379f56ef260a935b47f1c77f2f1f7470f7608d
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-22 23:47:26 +0200

    Add worried executives and shouting director

--- src/game/OfficeScene.ts
+++ src/game/OfficeScene.ts
@@ -20,6 +20,8 @@ export class OfficeScene extends Phaser.Scene {
   private ropeReady = false;
   private climbing = false;
   private executives: Phaser.GameObjects.Container[] = [];
+  private director!: Phaser.GameObjects.Container;
+  private directorBubble!: Phaser.GameObjects.Container;
   private rocketScreen!: Phaser.GameObjects.Container;
   private panelWire!: Phaser.GameObjects.Arc;
   private panelDoor!: Phaser.GameObjects.Container;
@@ -40,6 +42,7 @@ export class OfficeScene extends Phaser.Scene {
     this.drawOffice();
     this.createPlatforms();
     this.createExecutives();
+    this.createDirector();
     this.createControlPanel();
     this.createProjector();
     this.createUpperGarden();
@@ -171,6 +174,10 @@ export class OfficeScene extends Phaser.Scene {
       const eye2 = this.add.ellipse(6, -50, 5, 7, 0xffffff).setStrokeStyle(1, 0x302844);
       const pupil1 = this.add.circle(-6, -50, 1.5, 0x302844);
       const pupil2 = this.add.circle(6, -50, 1.5, 0x302844);
+      const brow1 = this.add.rectangle(-7, -57, 9, 2, 0x302844).setAngle(index % 2 ? 13 : -13);
+      const brow2 = this.add.rectangle(7, -57, 9, 2, 0x302844).setAngle(index % 2 ? -13 : 13);
+      const worriedMouth = this.add.arc(0, -39, 6, 195, 345, false, 0x7b4051).setStrokeStyle(2, 0x7b4051);
+      const sweat = this.add.ellipse(18, -55, 5, 9, 0x8edfff, 0.9).setAngle(12);
       const man = this.add
         .container(x, 486, [
           shadow,
@@ -191,6 +198,10 @@ export class OfficeScene extends Phaser.Scene {
           eye2,
           pupil1,
           pupil2,
+          brow1,
+          brow2,
+          worriedMouth,
+          sweat,
         ])
         .setDepth(9);
       this.executives.push(man);
@@ -202,6 +213,82 @@ export class OfficeScene extends Phaser.Scene {
         yoyo: true,
         repeat: -1,
       });
+    });
+  }
+
+  private createDirector(): void {
+    const x = 520;
+    const y = 258;
+    const shadow = this.add.ellipse(4, 42, 68, 13, 0x302844, 0.18);
+    const stickerTorso = this.add.rectangle(0, 8, 58, 76, 0xffffff);
+    const stickerHead = this.add.ellipse(0, -42, 45, 51, 0xffffff);
+    const stickerArm = this.add.rectangle(-35, -25, 24, 76, 0xffffff).setAngle(-28);
+    const torso = this.add.rectangle(0, 8, 48, 68, 0x222f50).setStrokeStyle(3, 0x302844);
+    const shoulder = this.add.ellipse(0, -11, 54, 25, 0x222f50).setStrokeStyle(2, 0x302844);
+    const shirt = this.add.triangle(0, -14, -11, 0, 11, 0, 0, 22, 0xf7f4ee);
+    const tie = this.add.triangle(0, 4, -6, -11, 6, -11, 0, 22, 0xd94867);
+    const raisedArm = this.add.rectangle(-32, -29, 16, 67, 0x222f50).setStrokeStyle(3, 0x302844).setAngle(-28);
+    const hand = this.add.circle(-48, -61, 10, 0xd99b72).setStrokeStyle(2, 0x302844);
+    const finger = this.add.rectangle(-50, -76, 7, 25, 0xd99b72).setStrokeStyle(2, 0x302844).setAngle(-5);
+    const neck = this.add.rectangle(0, -26, 15, 16, 0xd99b72);
+    const head = this.add.ellipse(0, -45, 37, 43, 0xd99b72).setStrokeStyle(3, 0x302844);
+    const hair = this.add.arc(0, -54, 19, 180, 360, false, 0x262332);
+    const brow1 = this.add.rectangle(-8, -51, 11, 3, 0x302844).setAngle(15);
+    const brow2 = this.add.rectangle(8, -51, 11, 3, 0x302844).setAngle(-15);
+    const eye1 = this.add.circle(-7, -45, 2.5, 0x302844);
+    const eye2 = this.add.circle(7, -45, 2.5, 0x302844);
+    const mouth = this.add.ellipse(0, -34, 12, 8, 0x6e2e3e).setStrokeStyle(2, 0x302844);
+    this.director = this.add
+      .container(x, y, [
+        shadow,
+        stickerArm,
+        stickerTorso,
+        stickerHead,
+        torso,
+        shoulder,
+        shirt,
+        tie,
+        raisedArm,
+        hand,
+        finger,
+        neck,
+        head,
+        hair,
+        brow1,
+        brow2,
+        eye1,
+        eye2,
+        mouth,
+      ])
+      .setDepth(14);
+
+    const bubbleShadow = this.add.ellipse(4, 5, 150, 58, 0x302844, 0.2);
+    const bubble = this.add.ellipse(0, 0, 150, 58, 0xffffff).setStrokeStyle(3, 0x302844);
+    const tail = this.add.triangle(-55, 28, -12, 0, 12, 0, 0, 30, 0xffffff).setStrokeStyle(3, 0x302844);
+    const text = this.add
+      .text(0, -4, "ON LANCE\nLA FUSÉE !", {
+        fontFamily: "system-ui",
+        fontSize: "13px",
+        fontStyle: "bold",
+        color: "#302844",
+        align: "center",
+        lineSpacing: 1,
+      })
+      .setOrigin(0.5);
+    this.directorBubble = this.add.container(x + 105, y - 98, [bubbleShadow, tail, bubble, text]).setDepth(15);
+    this.tweens.add({
+      targets: this.director,
+      angle: { from: -1, to: 1 },
+      duration: 420,
+      yoyo: true,
+      repeat: -1,
+    });
+    this.tweens.add({
+      targets: this.directorBubble,
+      scale: { from: 0.97, to: 1.03 },
+      duration: 520,
+      yoyo: true,
+      repeat: -1,
     });
   }
 
@@ -538,6 +625,17 @@ export class OfficeScene extends Phaser.Scene {
       this.tweens.add({ targets: man, y: man.y - 12, duration: 180, yoyo: true, delay: index * 80 });
       this.add.text(man.x, man.y - 95, "HA !", { fontSize: "13px", color: "#ffd36a" }).setOrigin(0.5);
     });
+    this.tweens.add({
+      targets: this.director,
+      angle: -8,
+      y: this.director.y + 8,
+      duration: 250,
+      ease: "Back.out",
+    });
+    const directorText = this.directorBubble.list.find(
+      (item) => item instanceof Phaser.GameObjects.Text,
+    ) as Phaser.GameObjects.Text | undefined;
+    directorText?.setText("QUOI ?!\nUN CHAT ?");
     this.objectiveText.setText("⇧ MONTER");
     this.showMessage("Ils adorent le chat !\nLe passage est libre", 2200);
   }
