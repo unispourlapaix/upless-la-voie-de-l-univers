486ec12b3a52c437715fceb8d4642e27f51f99b3
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-07-06 01:16:22 +0200

    Add PWA install icons and manifest

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "upless-app-v1";
+const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => undefined));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached ?? fetch(event.request).catch(() => caches.match("./"))),
+  );
+});
